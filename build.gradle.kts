0dd89fe69bea3b5aad8cdb42173d23499b2e5fff
bifidokk <bifidokk@gmail.com>
2022-10-25 20:03:44 +0300

    Remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
