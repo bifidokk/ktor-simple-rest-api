c5bc2a2103797f6b4fc985e45c1d77449c6352c4
bifidokk <bifidokk@gmail.com>
2022-11-01 20:09:53 +0200

    Show notes only for authorized user

--- src/main/kotlin/com/bifidokk/route/NoteRouter.kt
+++ src/main/kotlin/com/bifidokk/route/NoteRouter.kt
@@ -4,16 +4,19 @@ import com.bifidokk.repository.NoteRepository
 import com.bifidokk.service.CommonResponse
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.noteRouter(noteRepository: NoteRepository) {
-    get("/") {
-        val notes = noteRepository.getAllNotes()
-        call.respond(
-            HttpStatusCode.OK, CommonResponse(
-                data = notes
+    authenticate("auth-jwt") {
+        get("/") {
+            val notes = noteRepository.getAllNotes()
+            call.respond(
+                HttpStatusCode.OK, CommonResponse(
+                    data = notes
+                )
             )
-        )
+        }
     }
 }
